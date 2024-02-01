e931df6299bd86a00f48e23fb09b5f3728b51af6
Shaun Lennon <shaunlen99@gmail.com>
2024-02-01 01:57:52 +0000

    feat: add PrizeModel class for storing prize information

---
+++ RaffleKing/Data/Models/PrizeModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaffleKing.Data.Models;
+
+/// <summary>
+/// Represents an individual prize in a raffle.
+/// This model is used to store and manage details of each prize associated with a raffle.
+/// </summary>
+/// <param name="name">The name of the prize, max 30 characters.</param>
+/// <param name="description">A detailed description of the prize, max 500 characters.</param>
+/// <param name="quantity">The quantity of this prize available in the raffle.</param>
+/// <param name="raffleId">The identifier of the raffle to which this prize is associated.</param>
+public class PrizeModel
+    (string name, string description, int quantity, int raffleId)
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PrizeId { get; set; } // auto-generated
+
+    [Required]
+    [StringLength(30)]
+    public string Name { get; set; } = name;
+
+    [Required]
+    [StringLength(500)]
+    public string Description { get; set; } = description;
+
+    [Required]
+    public int Quantity { get; set; } = quantity;
+
+    [ForeignKey("RaffleModel")]
+    public int RaffleId { get; set; } = raffleId;
+
+    // Navigation property
+    public virtual RaffleModel? Raffle { get; set; }
+}
