f558237cb552f4c50bcafd8f440da299e9fd0861
Shaun Lennon <shaunlen99@gmail.com>
2024-02-01 22:19:45 +0000

    feat: add LeftNav blazor component

---
+++ RaffleKing/Components/Layout/LeftNav.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RaffleKing.Components.Layout;
+
+public partial class LeftNav
+{
+    [Parameter]
+    public bool IsDrawerClosed { get; set; }
+    private bool _isRafflesExpanded;
+    
+    /// <summary>
+    /// Closes any expanded MudNavGroup components within the MudNavMenu.
+    /// </summary>
+    public void CloseExpandedGroups()
+    {
+        _isRafflesExpanded = false;
+        StateHasChanged();
+    }
+    
+    /* Override methods */
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (IsDrawerClosed)
+            CloseExpandedGroups();
+    }
+}
