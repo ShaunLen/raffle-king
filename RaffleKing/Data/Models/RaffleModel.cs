83e15bb6f6f08f7857a036c2e5952d2433bc3834
Shaun Lennon <shaunlen99@gmail.com>
2024-02-01 01:26:09 +0000

    feat: add RaffleModel class for storing raffle information

---
+++ RaffleKing/Data/Models/RaffleModel.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaffleKing.Data.Models;
+
+/// <summary>
+/// Represents a raffle with its associated details.
+/// This model is used to store and manage data related to a raffle in the database.
+/// </summary>
+/// <param name="title">The title of the raffle, max 30 words.</param>
+/// <param name="description">A detailed description of the raffle, max 500 words.</param>
+/// <param name="drawDate">The scheduled draw date/time of the raffle.</param>
+/// <param name="isActive">Indicates whether the raffle is currently active.
+/// False upon creation, True when raffle is published and False when raffle has finished.</param>
+/// <param name="isBundle">Indicates whether the raffle is a bundle (one winner for all prizes) or not (one winner for
+/// each prize individually).</param>
+/// <param name="raffleHolderId">The identifier of the raffle holder, linking to an ApplicationUser.</param>
+public class RaffleModel
+    (string title, string description, DateTime drawDate, bool isActive, bool isBundle, string raffleHolderId)
+{
+    [Key] 
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+    public int RaffleId { get; set; } // auto-generated
+    
+    [Required] 
+    [StringLength(30)] 
+    public string Title { get; set; } = title;
+    
+    [Required]
+    [StringLength(500)]
+    public string Description { get; set; } = description;
+    
+    [Required]
+    public DateTime DrawDate { get; set; } = drawDate;
+
+    // Set to true when the raffle is published (after prizes are added)
+    [Required] 
+    public bool IsActive { get; set; } = false; 
+    
+    [Required]
+    public bool IsBundle { get; set; } = isBundle;
+    
+    [ForeignKey("AspNetUsers")]
+    public string RaffleHolderId { get; set; } = raffleHolderId;
+    
+    // Identity user reference
+    public virtual ApplicationUser? RaffleHolder { get; set; }
+}
