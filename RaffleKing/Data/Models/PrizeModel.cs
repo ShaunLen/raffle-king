116e04a3f23d6b557489dfa3f16259379fed9f0e
Shaun Lennon <shaunlen99@gmail.com>
2024-02-04 15:30:28 +0000

    fix: correct some small issues within PrizeModel

--- RaffleKing/Data/Models/PrizeModel.cs
+++ RaffleKing/Data/Models/PrizeModel.cs
@@ -17,29 +17,30 @@ public class PrizeModel
     /// </summary>
     [Required]
     [StringLength(30)]
-    public string Title { get; set; } = "Untitled Prize";
+    public string Title { get; set; } = null!;
 
     /// <summary>
     /// The description of the prize, up to 500 characters.
     /// </summary>
     [Required]
     [StringLength(500)]
-    public string Description { get; set; } = "Missing description";
+
+    public string Description { get; set; } = null!;
 
     /// <summary>
-    /// The quantity of this prize available in the raffle.
+    /// The quantity of this prize available in the draw.
     /// </summary>
     [Required]
     public int Quantity { get; init; } = 1;
 
     /// <summary>
-    /// The identifier of the raffle to which this prize is associated.
+    /// The identifier of the draw to which this prize is associated.
     /// </summary>
-    [ForeignKey("RaffleModel")]
-    public int RaffleId { get; init; }
+    [ForeignKey(nameof(Draw))]
+    public int DrawId { get; init; }
 
     /// <summary>
-    /// Navigation property to the associated raffle.
+    /// Navigation property to the associated draw.
     /// </summary>
-    public virtual RaffleModel? Raffle { get; set; }
+    public virtual DrawModel? Draw { get; set; }
 }
