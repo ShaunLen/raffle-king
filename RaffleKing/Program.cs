513d684cd91171d438a1071094e2ea00f1f61478
Shaun Lennon <shaunlen99@gmail.com>
2024-02-04 17:11:28 +0000

    feat: add Identity roles and default users

--- RaffleKing/Program.cs
+++ RaffleKing/Program.cs
@@ -32,7 +32,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
@@ -63,5 +64,63 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+// Set up Identity roles
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new[] { "User", "Host", "Admin" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+// Create default account for each role (testing purposes)
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    
+    // "User" account
+    if (await userManager.FindByEmailAsync("user@default.com") == null)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = "DefaultUser",
+            Email = "user@default.com"
+        };
+        
+        await userManager.CreateAsync(user, "User12345!");
+        await userManager.AddToRoleAsync(user, "User");
+    }
+    
+    // "Host" account
+    if (await userManager.FindByEmailAsync("host@default.com") == null)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = "DefaultHost",
+            Email = "host@default.com"
+        };
+        
+        await userManager.CreateAsync(user, "Host12345!");
+        await userManager.AddToRoleAsync(user, "Host");
+    }
+    
+    // "Admin" account
+    if (await userManager.FindByEmailAsync("admin@default.com") == null)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = "SiteAdmin",
+            Email = "admin@default.com"
+        };
+        
+        await userManager.CreateAsync(user, "Admin12345!");
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
 
 app.Run();
