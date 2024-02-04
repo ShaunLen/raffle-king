0af22a5f5ea54f56b6c19fad5bce36dbfbcae133
Shaun Lennon <shaunlen99@gmail.com>
2024-02-04 16:31:31 +0000

    feat: configure ApplicationDbContext

--- RaffleKing/Data/ApplicationDbContext.cs
+++ RaffleKing/Data/ApplicationDbContext.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RaffleKing.Data.Models;
 
 namespace RaffleKing.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<DrawModel> Draws { get; set; }
+    public DbSet<PrizeModel> Prizes { get; set; }
+    public DbSet<EntryModel> Entries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Override table names to singular
+        builder.Entity<DrawModel>().ToTable("Draw");
+        builder.Entity<PrizeModel>().ToTable("Prize");
+        builder.Entity<EntryModel>().ToTable("Entry");
+    }
 }
