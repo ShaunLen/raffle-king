b4b43f8eab89eb28d685ce02ee516f311ed91ae6
Shaun Lennon <shaunlen99@gmail.com>
2024-02-05 01:49:06 +0000

    feat: add AuthenticationService to handle authentication logic

---
+++ RaffleKing/Services/AuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using RaffleKing.Data;
+
+namespace RaffleKing.Services;
+
+public class AuthenticationService(SignInManager<ApplicationUser> signInManager, NavigationManager navigationManager)
+{
+    public async Task SignOutAsync()
+    {
+        await signInManager.SignOutAsync();
+        navigationManager.NavigateTo("/");
+    }
+}
