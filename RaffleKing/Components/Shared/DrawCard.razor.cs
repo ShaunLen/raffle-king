b69f8b8e42f52f96bef34c700ffcec9405fd937c
Shaun Lennon <shaunlen99@gmail.com>
2024-02-06 01:23:09 +0000

    feat: add placeholder DrawCard for use in DrawList

---
+++ RaffleKing/Components/Shared/DrawCard.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RaffleKing.Components.Shared;
+
+public partial class DrawCard
+{
+    [Parameter] public string Title { get; set; } = "Missing Title";
+    [Parameter] public string Description { get; set; } = "Missing description.";
+}
