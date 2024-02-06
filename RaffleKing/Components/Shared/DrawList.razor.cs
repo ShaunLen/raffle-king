add96920b58a74bf69530fa53b698cbbae7405e2
Shaun Lennon <shaunlen99@gmail.com>
2024-02-06 01:27:50 +0000

    feat: add DrawList component

---
+++ RaffleKing/Components/Shared/DrawList.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using RaffleKing.Data.Models;
+
+namespace RaffleKing.Components.Shared;
+
+public enum DrawListType
+{
+    ActiveDraws,
+    HostedDraws,
+    EnteredDraws
+}
+
+public partial class DrawList
+{
+    [Parameter]
+    public DrawListType ListType { get; set; }
+    
+    private List<DrawModel>? _draws;
+    private string? _headingPrefix;
+
+    protected override async Task OnInitializedAsync()
+    {
+        switch (ListType)
+        {
+            case DrawListType.ActiveDraws:
+                _headingPrefix = "Active";
+                break;
+            case DrawListType.HostedDraws:
+                _draws = await DrawService.GetHostedDraws();
+                _headingPrefix = "My";
+                break;
+            case DrawListType.EnteredDraws:
+                _headingPrefix = "Entered";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
