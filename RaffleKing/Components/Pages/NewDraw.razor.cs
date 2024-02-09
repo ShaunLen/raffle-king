0637d19fa3a96f42be5d707c4fbaeca9408f38b2
Shaun Lennon <shaunlen99@gmail.com>
2024-02-09 17:36:38 +0000

    feat: add NewDraw page for creating draws

---
+++ RaffleKing/Components/Pages/NewDraw.razor.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using MudBlazor;
+using RaffleKing.Data.Models;
+
+namespace RaffleKing.Components.Pages;
+
+public partial class NewDraw
+{
+    // Form Fields
+    private string? _title;
+    private string? _description;
+    private DrawTypeEnum _drawType;
+    private bool _isBundle;
+    private DateTime? _drawDate;
+    private TimeSpan? _drawTime;
+    private int _maxEntriesTotal = 50;
+    private int _maxEntriesPerUser = 5;
+
+    private async Task OnSubmit()
+    {
+        if (_title is null || _description is null)
+            return;
+        
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+        var userId = user.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        if (userId == null)
+        {
+            Snackbar.Add("User must be logged in to create a draw.", Severity.Error);
+            return;
+        }
+        
+        var newDraw = new DrawModel()
+        {
+            Title = _title,
+            Description = _description,
+            DrawDate = _drawDate!.Value.Add((TimeSpan) _drawTime!),
+            DrawType =_drawType,
+            IsBundle = _isBundle,
+            MaxEntriesTotal = _maxEntriesTotal,
+            MaxEntriesPerUser = _maxEntriesPerUser,
+            DrawHostId = userId
+        };
+            
+        try
+        {
+            await DrawService.AddNewDraw(newDraw);
+            Snackbar.Add("Draw created successfully.", Severity.Success);
+        }
+        catch (Exception exception)
+        {
+            Snackbar.Add($"Draw could not be created: {exception.Message}" , Severity.Error);
+        }
+    }
+}
