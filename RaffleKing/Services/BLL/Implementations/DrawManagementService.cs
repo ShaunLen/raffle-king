a82e5f46ee682807addfffbb079553f825c21958
Shaun Lennon <shaunlen99@gmail.com>
2024-02-21 01:04:48 +0000

    feat: implement IDrawManagementService

--- RaffleKing/Services/BLL/Implementations/DrawManagementService.cs
+++ RaffleKing/Services/BLL/Implementations/DrawManagementService.cs
@@ -1,8 +1,33 @@
-﻿using RaffleKing.Services.BLL.Interfaces;
+﻿using RaffleKing.Common;
+using RaffleKing.Data.Models;
+using RaffleKing.Services.BLL.Interfaces;
+using RaffleKing.Services.DAL.Interfaces;
 
 namespace RaffleKing.Services.BLL.Implementations;
 
-public class DrawManagementService : IDrawManagementService
+public class DrawManagementService(IDrawService drawService, IPrizeManagementService prizeManagementService,
+    IPrizeService prizeService, IEntryService entryService) 
+    : IDrawManagementService
 {
-    
+    public async Task<DrawModel?> GetDrawById(int drawId)
+    {
+        return await drawService.GetDrawById(drawId);
+    }
+
+    public async Task<OperationResult> ActivateDraw(int drawId)
+    {
+        var prizeCount = await prizeService.CountPrizesByDraw(drawId);
+        if(prizeCount == 0)
+            return OperationResult.Fail("Cannot publish a draw that has no prizes.");
+
+        await drawService.ActivateDraw(drawId);
+        return OperationResult.Ok();
+    }
+
+    public async Task DeleteDraw(int drawId)
+    {
+        await prizeService.DeletePrizesByDraw(drawId);
+        await entryService.DeleteEntriesByDraw(drawId);
+        await drawService.DeleteDraw(drawId);
+    }
 }
--- RaffleKing/Services/BLL/Interfaces/IDrawManagementService.cs
+++ RaffleKing/Services/BLL/Interfaces/IDrawManagementService.cs
@@ -1,6 +1,11 @@
-﻿namespace RaffleKing.Services.BLL.Interfaces;
+﻿using RaffleKing.Common;
+using RaffleKing.Data.Models;
+
+namespace RaffleKing.Services.BLL.Interfaces;
 
 public interface IDrawManagementService
 {
-    
+    Task<DrawModel?> GetDrawById(int drawId);
+    Task<OperationResult> ActivateDraw(int drawId);
+    Task DeleteDraw(int drawId);
 }
