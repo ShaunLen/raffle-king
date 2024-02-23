9575ed6d3fb3b49b19c954887a50a9301fbe8dd7
Shaun Lennon <shaunlen99@gmail.com>
2024-02-23 22:50:15 +0000

    chore: refactor DrawDetails page to split into components

---
+++ RaffleKing/Components/Pages/DrawDetails/DrawDetails.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using RaffleKing.Data.Models;
+
+namespace RaffleKing.Components.Pages.DrawDetails;
+
+public partial class DrawDetails
+{
+    /// <summary>
+    /// The Id of the draw to be displayed.
+    /// </summary>
+    [Parameter]
+    public int DrawId { get; set; }
+
+    private DrawModel? _draw;
+    private List<PrizeModel>? _prizes;
+    private bool _userIsHost;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _draw = await DrawManagementService.GetDrawById(DrawId);
+        _prizes = await PrizeManagementService.GetPrizesByDraw(DrawId);
+        _userIsHost = await UserService.IsHostOfDraw(DrawId);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+            await SnackbarHelper.DisplayPendingSnackbarMessages();
+    }
+}
