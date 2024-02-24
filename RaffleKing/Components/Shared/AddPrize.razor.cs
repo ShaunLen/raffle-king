e0d2bddbb2e5f1ecbc23180929fbf21712594338
Shaun Lennon <shaunlen99@gmail.com>
2024-02-24 18:36:06 +0000

    chore: refactor AddPrize dialog to replace DAL usage with BLL usage

--- RaffleKing/Components/Shared/AddPrize.razor.cs
+++ RaffleKing/Components/Shared/AddPrize.razor.cs
@@ -13,26 +13,22 @@ public partial class AddPrize
     private List<PrizeModel>? _prizes;
         
     // Form fields
-    private string? _title;
-    private string? _description;
+    private string _title = string.Empty;
+    private string _description = string.Empty;
     private int _quantity = 1;
 
     protected override async Task OnInitializedAsync()
     {
-        _draw = await DrawService.GetDrawById(DrawId);
-        _prizes = await PrizeService.GetPrizesForDraw(DrawId);
+        _draw = await DrawManagementService.GetDrawById(DrawId);
+        _prizes = await PrizeManagementService.GetPrizesByDraw(DrawId);
     }
 
     private void Cancel() => MudDialog.Close(DialogResult.Cancel());
 
     private void CreatePrize()
     {
-        if(!PrizeIsValid()) return;
-
-        if (_title == "")
-        {
-            Snackbar.Add("Title cannot be blank", Severity.Error);
-        }
+        if (!PrizeIsValid())
+            return;
         
         var prize = new PrizeModel
         {
@@ -47,18 +43,12 @@ public partial class AddPrize
 
     private bool PrizeIsValid()
     {
-        if (_title is null || _description is null)
-            return false;
-
         if (_title.Length == 0 || _description.Length == 0)
             return false;
         
         var currentPrizeQuantity = 0;
         if (_prizes != null) currentPrizeQuantity += _prizes.Sum(prize => prize.Quantity);
 
-        if (_draw is { IsBundle: false } && (currentPrizeQuantity + _quantity) > _draw.MaxEntriesTotal)
-            return false;
-
-        return true;
+        return _draw is not { IsBundle: false } || (currentPrizeQuantity + _quantity) <= _draw.MaxEntriesTotal;
     }
 }
