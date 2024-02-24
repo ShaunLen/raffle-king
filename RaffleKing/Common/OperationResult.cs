cdcb9d43ff52a7b7b09ddb3f28114799ef34f08e
Shaun Lennon <shaunlen99@gmail.com>
2024-02-24 16:12:06 +0000

    feat: add OperationResult variant to return data with result

--- RaffleKing/Common/OperationResult.cs
+++ RaffleKing/Common/OperationResult.cs
@@ -2,15 +2,24 @@
 
 public class OperationResult
 {
-    public bool Success { get; private set; }
-    public string Message { get; private set; } = string.Empty;
+    public bool Success { get; protected init; }
+    public string Message { get; protected init; } = string.Empty;
     
     // Factory method for a successful operation
-    public static OperationResult Ok(string message = "") => new OperationResult { Success = true, Message = message };
+    public static OperationResult Ok(string message = "") => new() { Success = true, Message = message };
     
     // Factory method for a failed operation
-    public static OperationResult Fail(string message) => new OperationResult { Success = false, Message = message };
+    public static OperationResult Fail(string message) => new() { Success = false, Message = message };
     
     // Private constructor to enforce use of factory methods
-    private OperationResult() {}
+    protected OperationResult() {}
+}
+
+// Variant of OperationResult for returning data with result
+public class OperationResult<T> : OperationResult
+{
+    public T? Data { get; private set; }
+    public static OperationResult<T> Ok(T data) => new() { Success = true, Data = data };
+    public static OperationResult<T> Fail(string message, T data) => new OperationResult<T>
+        { Success = false, Message = message, Data = data };
 }
