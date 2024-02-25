5ba876e09463e73d7942444fe0c82055d467ebe2
Shaun Lennon <shaunlen99@gmail.com>
2024-02-25 15:04:23 +0000

    feat: add WinnerModel for storing winners in database

---
+++ RaffleKing/Data/Models/WinnerModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RaffleKing.Data.Models;
+
+public class WinnerModel
+{
+    /// <summary>
+    /// Auto-generated primary key for the winning entry.
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; private init; }
+    
+    /// <summary>
+    /// The identifier of the winning entry.
+    /// </summary>
+    [ForeignKey(nameof(Entry))]
+    public int DrawId { get; init; }
+    
+    /// <summary>
+    /// The identifier of the prize won.
+    /// </summary>
+    [ForeignKey(nameof(Prize))]
+    public int PrizeId { get; init; }
+    
+    /// <summary>
+    /// Navigation property to the associated entry.
+    /// </summary>
+    public virtual EntryModel? Entry { get; set; }
+    
+    /// <summary>
+    /// Navigation property to the associated prize.
+    /// </summary>
+    public virtual PrizeModel? Prize { get; set; }
+}
