39f454d9ee881096c0385e79b3419c4ae45f4b81
Shaun Lennon <shaunlen99@gmail.com>
2024-04-01 15:14:35 +0100

    feat: add EnteredDraws page with functionality to enter guest reference

---
+++ RaffleKing/Components/Pages/EnteredDraws.razor.cs
@@ -0,0 +1,21 @@
+﻿using MudBlazor;
+
+namespace RaffleKing.Components.Pages;
+
+public partial class EnteredDraws
+{
+    private string _guestRef;
+
+    private async Task LoadDrawFromGuestRef()
+    {
+        var entry = await EntryManagementService.GetGuestEntry(_guestRef);
+        if (entry == null)
+        {
+            Snackbar.Add("No entry found with this guest reference code!", Severity.Error);
+            return;
+        }
+        
+        var drawId = entry.DrawId;
+        NavigationManager.NavigateTo($"/draws/draw-details/{drawId}");
+    }
+}
