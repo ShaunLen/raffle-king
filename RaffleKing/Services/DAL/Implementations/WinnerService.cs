5d6623cbebe7184570ecea977acbe1779e6c9334
Shaun Lennon <shaunlen99@gmail.com>
2024-04-01 18:10:35 +0100

    feat: send emails to winners when adding them to the Winner table

--- RaffleKing/Services/DAL/Implementations/WinnerService.cs
+++ RaffleKing/Services/DAL/Implementations/WinnerService.cs
@@ -1,18 +1,46 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RaffleKing.Data;
 using RaffleKing.Data.Models;
+using RaffleKing.Services.BLL.Interfaces;
 using RaffleKing.Services.DAL.Interfaces;
 
 namespace RaffleKing.Services.DAL.Implementations;
 
-public class WinnerService(IDbContextFactory<ApplicationDbContext> factory, IHttpContextAccessor httpContextAccessor) 
-    : IWinnerService
+public class WinnerService(IDbContextFactory<ApplicationDbContext> factory, IHttpContextAccessor httpContextAccessor,
+    IEntryService entryService, IEmailService emailService) : IWinnerService
 {
     public async Task AddWinner(WinnerModel winnerModel)
     {
         await using var context = await factory.CreateDbContextAsync();
         context.Winners.Add(winnerModel);
         await context.SaveChangesAsync();
+        
+        // Send email to winner below
+        if(winnerModel.EntryId == null)
+            return;
+        
+        var entryId = (int) winnerModel.EntryId;
+        var entry = await entryService.GetEntryById(entryId);
+
+        if (entry == null)
+            return;
+        
+        if (entry.IsGuest)
+        {
+            // Suppressed nullable warning as I know GuestEmail will not be null for a guest entry at this point
+            emailService.SendGuestWinnerEmail(entry.GuestEmail!);
+        }
+        else
+        {
+            var user = entry.User;
+            if(user == null)
+                return;
+
+            Console.WriteLine();
+            
+            // Suppressed nullable warning as I know that valid users will have a non-null Email
+            emailService.SendUserWinnerEmail(user.Email!);
+        }
     }
 
     public async Task<List<WinnerModel>?> GetWinnersByDraw(int drawId)
