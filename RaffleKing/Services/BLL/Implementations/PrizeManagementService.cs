3b5a8485377d4dc89d58e9148350df81d801ed7c
Shaun Lennon <shaunlen99@gmail.com>
2024-04-04 21:32:11 +0100

    feat: add two methods to IPrizeManagementService

--- RaffleKing/Services/BLL/Implementations/PrizeManagementService.cs
+++ RaffleKing/Services/BLL/Implementations/PrizeManagementService.cs
@@ -4,7 +4,7 @@ using RaffleKing.Services.DAL.Interfaces;
 
 namespace RaffleKing.Services.BLL.Implementations;
 
-public class PrizeManagementService(IPrizeService prizeService)
+public class PrizeManagementService(IPrizeService prizeService, IWinnerService winnerService, IUserService userService)
     : IPrizeManagementService
 {
     public async Task AddNewPrize(PrizeModel prizeModel)
@@ -20,6 +20,20 @@ public class PrizeManagementService(IPrizeService prizeService)
     public async Task<List<PrizeModel>?> GetPrizesByDraw(int drawId)
     {
         return await prizeService.GetPrizesByDraw(drawId);
+    }
+
+    public async Task<List<WinnerModel>?> GetUnclaimedPrizesByCurrentUser()
+    {
+        var currentUserId = await userService.GetUserId();
+        if (currentUserId == null)
+            return null;
+
+        return await winnerService.GetUnclaimedPrizesByUser(currentUserId);
+    }
+
+    public async Task ClaimPrize(int winnerId)
+    {
+        await winnerService.SetClaimed(winnerId);
     }
 
     public async Task DeletePrize(int prizeId)
--- RaffleKing/Services/BLL/Interfaces/IPrizeManagementService.cs
+++ RaffleKing/Services/BLL/Interfaces/IPrizeManagementService.cs
@@ -7,5 +7,7 @@ public interface IPrizeManagementService
     Task AddNewPrize(PrizeModel prizeModel);
     Task<PrizeModel?> GetPrizeById(int prizeId);
     Task<List<PrizeModel>?> GetPrizesByDraw(int drawId);
+    Task<List<WinnerModel>?> GetUnclaimedPrizesByCurrentUser();
+    Task ClaimPrize(int winnerId);
     Task DeletePrize(int prizeId);
 }
